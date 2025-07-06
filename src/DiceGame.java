b9c4e73a185c3a78bdb1aa6597d65caf7c04e3ce
Ben Aderaw <asyncba@gmail.com>
2025-07-05 20:24:48 -0400

    Add player score tracker

--- src/DiceGame.java
+++ src/DiceGame.java
@@ -2,37 +2,59 @@ import java.util.Scanner;
 
 public class DiceGame {
 
+    // OBJECTIVES
+    // add players 🟢
+    // player input 🟢
+    // implement simple player turn logic 🟢
+    // track rolls results  🟢
+    // implement win/lose 🔴
+    // implement full player turn logic 🔴
+
     //Objects
     Scanner scanner = new Scanner(System.in);
     Dice dice = new Dice();
 
     // FIELDS
     private String player;
+    private int targetNum;
     private int diceOneResult;
     private int diceTwoResult;
+    private int playerOneScore;
+    private int playerTwoScore;
 
     public void startGame(){
         System.out.println("\n<=== GAME STARTED ===>");
-        System.out.println(dice.targetNum());
+        // FIELDS
         player = "player1";
         String player1Input = "";
         String player2Input = "";
 
+        // get score target
+        targetNum = dice.targetNum();
 
+
+        // player turns
         while(true){
-
-            if(player.equals("player1")){
-                System.out.println("=== TURN: PLAYER 1 ===");
+            if (player.equals("player1")){
+                System.out.println("Turn: Player 1");
+                System.out.println("🎯 " + targetNum + "     Player One: " + playerOneScore + "     Player Two: " + playerTwoScore);
                 System.out.print("What would you like to do, roll / stop: ");
                 player1Input = scanner.nextLine().toLowerCase().trim();
 
                 while(true){
                     switch (player1Input){
                         case "roll":
+                            // roll two dice
                             diceOneResult = dice.rollDice();
                             diceTwoResult = dice.rollDice();
-                            System.out.println(diceOneResult);
-                            System.out.println(diceTwoResult);
+                            System.out.println("🎲 Dice One: " + diceOneResult);
+                            System.out.println("🎲 Dice Two: " + diceTwoResult);
+
+                            // get dice result sum
+                            int rollPoints = roleSum(diceOneResult, diceTwoResult);
+                            playerOneScore = playerOneScore + roleSum(diceOneResult, diceTwoResult);
+                            System.out.println("+" + rollPoints);
+
                             break;
                         case "stop":
                             break;
@@ -44,9 +66,10 @@ public class DiceGame {
                     break;
                 }
             }else if(player.equals("player2")){
-                System.out.println("=== TURN: PLAYER 2 ===");
+                System.out.println("Turn: Player 2");
+                System.out.println("🎯 " + targetNum + "     player One: " + playerOneScore + "     player Two: " + playerTwoScore);
                 System.out.print("What would you like to do, roll / stop: ");
-                player2Input = scanner.nextLine();
+                player2Input = scanner.nextLine().toLowerCase().trim();
 
                 while(true){
                     switch (player2Input){
@@ -55,6 +78,12 @@ public class DiceGame {
                             diceTwoResult = dice.rollDice();
                             System.out.println(diceOneResult);
                             System.out.println(diceTwoResult);
+
+                            // get dice result sum
+                            int rollPoints = roleSum(diceOneResult, diceTwoResult);
+                            playerTwoScore = playerTwoScore + roleSum(diceOneResult, diceTwoResult);
+                            System.out.println("+" + rollPoints);
+
                             break;
                         case "stop":
                             break;
@@ -73,10 +102,11 @@ public class DiceGame {
 
             player = (player.equals("player1")) ? "player2" : "player1";
         }
+    }
 
-
-
-
+    // METHODS
+    private int roleSum(int diceOneResult, int diceTwoResult){
+        return (diceOneResult + diceTwoResult);
     }
 
 }
